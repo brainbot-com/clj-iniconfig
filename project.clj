2454c1af96e85f6204cc8d239ceeb33b4e381f06
Ralf Schmitt <ralf@systemexit.de>
2013-06-16 12:53:02 +0200

    add :scm key to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/brainbot-com/clj-iniconfig"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url https://github.com/brainbot-com/clj-iniconfig.git}
   :dependencies [[org.clojure/clojure "1.5.1"]])
