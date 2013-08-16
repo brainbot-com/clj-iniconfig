af0f2a3e0be0a25a7953234df1b568f6272dabd3
Ralf Schmitt <ralf@systemexit.de>
2013-08-16 15:02:01 +0200

    add ability to test with multiple clojure versions
    just run 'lein all test'

--- project.clj
+++ project.clj
@@ -5,4 +5,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url https://github.com/brainbot-com/clj-iniconfig}
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+
+  :aliases {"all" ["with-profile" "dev:+1.4:+1.6"]}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
+
   :dependencies [[org.clojure/clojure "1.5.1"]])
